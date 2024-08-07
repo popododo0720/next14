95b2faa18a97c9bbedeffe0d2cb86d9dd76b23db
unknown <popododo0720@naver.com>
2024-08-07 10:20:44 +0900

    first

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { API_URL } from "../../../(home)/page"
+
+async function getMovie(id:string){
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id: string){
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({
+    params: { id },
+    }: {
+        params: { id: string };
+    }) {
+    
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+    return <h1>{movie.title}</h1>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import Navigation from "../components/navigation"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Loading...",
+  },
+  description: "Best movies",
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+        </body>
+    </html>
+  )
+}
